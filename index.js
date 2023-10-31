4b5fec9580053ef878fed69b263454a43ae2be78
Sandy-1711 <sandy1711003@gmail.com>
2023-10-31 14:21:21 +0530

    new prompt for symptoms added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const dotenv = require('dotenv')
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const data = require('./data')
+// const data = require('./data')
 dotenv.config();
 const app = express();
 app.use(cors());
@@ -26,9 +26,6 @@ app.get('/sih/api/:muscle/info', async function (req, res) {
 
     prompt = `Act as an experienced myologist, who has more than 10 years of experience in the field, and answer my queries.Give me a simplified description of ${muscle} muscle, which even a 10 year old kid can understand and the information should be trustworthy.`;
     try {
-        // if (muscle !== 'abdominals' && muscle !== 'obliques' && muscle !== 'forearms' && muscle !== 'biceps' && muscle !== 'shoulders') {
-        //     res.status(404).json("Not Found");
-        // }
 
         const response = await openai.createCompletion({
             model: "text-davinci-003",
@@ -37,14 +34,7 @@ app.get('/sih/api/:muscle/info', async function (req, res) {
             temperature: 1,
         });
         const data = response.data.choices[0].text.trim();
-        // console.log(data.text);
-        // const parsedResponse = await JSON.parse(data);
-        // console.log(parsedResponse);
-        // var cleanedString = data.text.replace(/[\n\\]/g, '');
 
-        // cleanedString=cleanedString.substring(1,cleanedString.length-1);
-        //  cleanedString = data.text.replace(/\/g, '');
-        // console.log(cleanedString.Answer);
         const responseObject = {
             muscle: muscle,
             info: data
@@ -60,41 +50,33 @@ app.get('/sih/api/:muscle/info', async function (req, res) {
 
 
 })
-app.get('/sih/api/:muscle/:symptom', function (req, res) {
+app.get('/sih/api/:muscle/symptoms', async function (req, res) {
     const muscle = req.params.muscle.toLowerCase().trim();
-    const symptom = req.params.symptom.toLowerCase().trim();
-    if (muscle !== 'abdominals' && muscle !== 'obliques' && muscle !== 'forearms' && muscle !== 'biceps' && muscle !== 'shoulders') {
-        res.status(404).json("Not Found");
-    }
-    if (symptom !== 'weakness' && symptom !== 'pain' && symptom !== 'cramps' && symptom !== 'twitching' && symptom !== 'fatigue') {
-        res.status(404).json("Please Select Right Symptom");
-    }
+    // const symptom = req.params.symptoms.toLowerCase().trim();
+    // if (muscle !== 'abdominals' && muscle !== 'obliques' && muscle !== 'forearms' && muscle !== 'biceps' && muscle !== 'shoulders') {
+    //     res.status(404).json("Not Found");
+    // }
+    // if (symptom !== 'weakness' && symptom !== 'pain' && symptom !== 'cramps' && symptom !== 'twitching' && symptom !== 'fatigue') {
+    //     res.status(404).json("Please Select Right Symptom");
+    // }
+    prompt = `you are my health advisor,i will give you muscle name based on that you have to give me 10 common medical symptoms of that muscle. muscle=${muscle}. The response that you will provide should be in JSON Stringified object format. Example : symptoms:[{ symptom:'Pain',},{symptom:'Swelling',},{symptom:'Itching',},]`;
     try {
-
-        const foundMuscle = data.muscles.filter(function (x) {
-            return x.name.toLowerCase() === muscle;
-        })
-        if (foundMuscle[0]) {
-
-            const response = {
-                name: muscle,
-                description: foundMuscle[0].description,
-                remedies: foundMuscle[0].remedies[symptom],
-            }
-
-            if (response) {
-                res.status(200).json(response);
-            }
-        }
-        else {
-            res.status(404).json("Not Found");
-        }
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: prompt,
+            max_tokens: 2048,
+            temperature: 1,
+        });
+        const data = response.data.choices[0].text.trim();
+        console.log(data);
+        res.status(200).json(data);
     }
     catch (err) {
         res.status(500).json(err);
     }
 
 })
+
 app.listen(8080, function () {
     console.log("server running at port 8080");
 })
